f42f37848debde3d838b9b8c77438acbc4b0e3fc
JayArindamMaity <maityjayarindam@gmail.com>
2025-08-08 23:33:09 +0530

    idk what im doing

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -3,7 +3,9 @@ import './home.css'
 export default function () {
     return (
         <>
-            this  is the home page
+            Welcome to the homepage of rabbitcode!
+            <br />A website curated for you to find solutions to existing programming problems
+            <br />here is how you can use this website
         </>
     )
 }
